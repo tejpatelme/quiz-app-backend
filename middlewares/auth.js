070b8005b710069d0ae2e7a03e0891d3fe60e15d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-10 15:55:18 +0000

    create : authentication middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  const isEmail = validator.isEmail(email);
+
+  if (!isEmail) {
+    return res
+      .status(401)
+      .json({ success: false, errorMessage: "Email is invalid" });
+  }
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  const isStrongPassword = validator.isStrongPassword(password,{minSymbols: 0});
+
+  if (!isStrongPassword) {
+    return res
+      .status(401)
+      .json({ success: false, errorMessage: "Password is not strong enough" });
+  }
+
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  req.userId = decodedToken.userId;
+
+  next();
+};
+
+module.exports = { validateEmail, validatePassword, verifyToken };
